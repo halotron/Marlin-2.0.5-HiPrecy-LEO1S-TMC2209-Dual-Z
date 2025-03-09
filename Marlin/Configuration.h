320b76155d71818d66541f26444006bacc2a0dc6
Jonas Jakobsson <jonas@knacka.se>
2025-03-09 14:47:01 +0100

    mr msyasd

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -729,14 +729,14 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 200, 415 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 200, 415 }  
 
 /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 8, 100 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 150, 50 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
